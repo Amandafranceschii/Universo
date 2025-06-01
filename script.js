5c88ab7fbd8738c3fe21e80cf7118ce2bd24a16d
Amandafranceschii <jdsupplycenter@gmail.com>
2025-06-01 20:41:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function verificarSenha() {
-  const senha = document.getElementById("senha").value;
-  if (senha === "2103") {
-    document.getElementById("senha-container").style.display = "none";
-    document.getElementById("conteudo").style.display = "block";
-  } else {
-    document.getElementById("mensagem-erro").innerText = "Senha incorreta 😢";
-  }
-}
+// Sem necessidade de JS nessa versão simplificada
